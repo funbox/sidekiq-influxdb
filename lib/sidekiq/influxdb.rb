33891adce3e8541cac7cfa7d6be75cc8a8d68c8f
Ilya Vassilevsky <vassilevsky@gmail.com>
2017-11-26 15:36:25 +0400

    bundle gem

---
+++ lib/sidekiq/influxdb.rb
@@ -0,0 +1,7 @@
+require "sidekiq/influxdb/version"
+
+module Sidekiq
+  module Influxdb
+    # Your code goes here...
+  end
+end
---
+++ lib/sidekiq/influxdb/version.rb
@@ -0,0 +1,5 @@
+module Sidekiq
+  module Influxdb
+    VERSION = "0.1.0"
+  end
+end
